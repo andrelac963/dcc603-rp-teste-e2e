aa3e4a6fe5869234a619daf91f4a6316a861e7f4
André Luiz Alves <andrelac963@gmail.com>
2025-06-22 21:48:35 -0300

    Adicionando mais três testes no spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -68,4 +68,74 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+
+  it('Edita uma tarefa existente', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('TP2 de Engenharia de Software{enter}');
+
+    cy.get('[data-cy=todos-list] li label')
+      .dblclick();
+
+    cy.get('[data-cy=todos-list] li.editing .edit')
+      .clear()
+      .type('TP3 de Engenharia de Software{enter}');
+
+    cy.get('[data-cy=todos-list] li')
+      .should('have.length', 1)
+      .first()
+      .should('contain.text', 'TP3 de Engenharia de Software');
+  });
+
+  it('Limpa todas as tarefas concluídas', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('Tarefa 1{enter}')
+      .type('Tarefa 2{enter}')
+      .type('Tarefa 3{enter}');
+
+    cy.get('[data-cy=todos-list] li .toggle')
+      .eq(0)
+      .click();
+    cy.get('[data-cy=todos-list] li .toggle')
+      .eq(1)
+      .click();
+
+    cy.get('[data-cy=todos-list] li.completed').should('have.length', 2);
+
+    cy.get('.clear-completed').click();
+
+    cy.get('[data-cy=todos-list] li')
+      .should('have.length', 1)
+      .first()
+      .should('contain.text', 'Tarefa 3');
+  });
+
+  it('Alterna o estado de uma tarefa entre ativa e concluída', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('Tarefa de teste{enter}');
+
+    cy.get('[data-cy=todos-list] li')
+      .should('have.length', 1)
+      .first()
+      .should('not.have.class', 'completed');
+
+    cy.get('[data-cy=todos-list] li .toggle')
+      .click();
+
+    cy.get('[data-cy=todos-list] li')
+      .first()
+      .should('have.class', 'completed');
+
+    cy.get('[data-cy=todos-list] li .toggle')
+      .click();
+
+    cy.get('[data-cy=todos-list] li')
+      .first()
+      .should('not.have.class', 'completed');
+  });
 });
